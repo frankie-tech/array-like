5d1215a1f46ea360455705290537793b02764d61
frankie <26461046+frankiekie@users.noreply.github.com>
2020-08-26 14:33:33 -0400

    adds some basic types to try and fix reduce error

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+type ReduceValue = number | string | Array<any>;
+
+type ReduceCallback = (
+	prevValue: any,
+	currValue: any,
+	currIndex: number,
+	array: any[] | ArrayLike<any>
+) => any;
+
+/*
+export class Arrayish<T> {
+	length: number;
+	reduce: { (callbackfn: (arg0: ReduceCallback) => ReduceValue): ReduceValue }
+}
+*/
