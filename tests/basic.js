41f6c621e6be612c0cdc737eddebba71e0cb2efc
frankie <26461046+frankiekie@users.noreply.github.com>
2020-08-26 14:33:10 -0400

    updates tests to include options

--- tests/basic.js
+++ tests/basic.js
@@ -1,21 +1,21 @@
 import { test } from 'uvu';
 import assert from 'uvu/assert';
-import ArrayLike from '../src/index';
+import ArrayLike from '../dist/index.modern';
 
 test('species', () => {
-	const arrayLike = new ArrayLike(1, 2, 3, 4);
+	const arrayLike = new ArrayLike({ unique: false }, 1, 2, 3, 4);
 	assert.type(arrayLike, 'object');
 	assert.instance(arrayLike, Array);
 });
 
 test('value', () => {
-	const arrayLike = new ArrayLike(1, 2, 3, 4);
+	const arrayLike = new ArrayLike({ unique: false }, 1, 2, 3, 4);
 	assert.is(arrayLike, arrayLike);
 	assert.is.not(arrayLike, [1, 2, 3, 4]);
 });
 
 test('length', () => {
-	const arrayLike = new ArrayLike(1, 2, 3, 4);
+	const arrayLike = new ArrayLike({ unique: false }, 1, 2, 3, 4);
 	assert.equal(arrayLike.length, [1, 2, 3, 4].length);
 	assert.equal(arrayLike.last, 4);
 });
