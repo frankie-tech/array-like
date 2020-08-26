217c26c5c67f120e3866c17f186343f2810a3609
frankie <26461046+frankiekie@users.noreply.github.com>
2020-08-26 15:34:25 -0400

    gets tests to pass

--- tests/basic.js
+++ tests/basic.js
@@ -1,21 +1,22 @@
 import { test } from 'uvu';
 import assert from 'uvu/assert';
-import ArrayLike from '../dist/index.modern';
+import Arrayish from '../dist/array-ish.modern';
 
 test('species', () => {
-	const arrayLike = new ArrayLike({ unique: false }, 1, 2, 3, 4);
+	const arrayLike = new Arrayish({ unique: false }, 1, 2, 3, 4);
+
 	assert.type(arrayLike, 'object');
 	assert.instance(arrayLike, Array);
 });
 
 test('value', () => {
-	const arrayLike = new ArrayLike({ unique: false }, 1, 2, 3, 4);
+	const arrayLike = new Arrayish({ unique: false }, 1, 2, 3, 4);
 	assert.is(arrayLike, arrayLike);
 	assert.is.not(arrayLike, [1, 2, 3, 4]);
 });
 
 test('length', () => {
-	const arrayLike = new ArrayLike({ unique: false }, 1, 2, 3, 4);
+	const arrayLike = new Arrayish({ unique: false }, 1, 2, 3, 4);
 	assert.equal(arrayLike.length, [1, 2, 3, 4].length);
 	assert.equal(arrayLike.last, 4);
 });
